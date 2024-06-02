ae666e3f8133879455e6dbf89a2176408876aba5
Anton Danilov <anton2009danilov@yandex.ru>
2024-06-02 14:41:37 +0300

    fixes

--- brain_games/games/brain_games.py
+++ brain_games/games/brain_games.py
@@ -4,14 +4,18 @@ import prompt
 
 def get_user_name():
     user_name = prompt.string(
-        'Welcome to the Brain Games!\n'
+        # 'Welcome to the Brain Games!\n'
         'May I have you name? ', empty=True
     )
     return user_name if user_name is not None else 'Anonymous'
 
 
 def main():
-    # print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string(
+        'May I have you name? ',
+        empty=True,
+    )
     user_name = get_user_name()
     print(f'Hello, {user_name}!')
 
